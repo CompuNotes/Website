f9fa279646ea659c5e5ce1e496b843eff455a39c
Nuhacet Travieso del Rosario <nuhacet66@gmail.com>
2025-04-08 16:53:36 +0100

    Agregar funcionalidad de inicio de sesión y registro de usuario, eliminando archivos innecesarios

---
+++ Backend/Login_access.js
@@ -0,0 +1,18 @@
+
+async function loginUser(username, password) {
+    console.log("inicio de la función loginUser");
+    try {
+        const response = await axios.post("https://django.narurm.eu/compunotesapi/user/", {
+            username: username,
+            password: password
+        });
+
+      
+
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        console.log("Login exitoso, token:", token);
+    } catch (error) {
+        console.error("Error en el login:", error.response?.data || error.message);
+    }
+}
