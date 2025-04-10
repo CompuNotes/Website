3a3b1b6ec303bd5793036a9a19db48b1d92694bc
raulm1906 <113340217+raulm1906@users.noreply.github.com>
2025-04-10 12:15:56 +0100

    Config deployment server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://compunotes.narurm.eu',
+    server: {
+        allowedHosts: ['compunotes.narurm.eu'],
+        host: true,
+        port: 7000
+    },
+});
