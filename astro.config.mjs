381c696eebecbd3c901a020ceb2aeeef4ae4bfc5
raulm1906 <113340217+raulm1906@users.noreply.github.com>
2025-04-29 18:46:35 +0100

    Server deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://compunotes.narurm.eu',
-    server: {
-        allowedHosts: ['compunotes.narurm.eu'],
-        host: true,
-        port: 7000
+  site: 'https://compunotes.narurm.eu',
+
+  server: {
+      allowedHosts: ['compunotes.narurm.eu'],
+      host: true,
+      port: 7001
+  },
+
+  trailingSlash: 'never',
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+    experimental: {
+      session: true
     },
 });
